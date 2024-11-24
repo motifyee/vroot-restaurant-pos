9f5d4f2e0680f7504b208c23adaacc2d063939d1
mrt <iquixe@gmail.com>
2024-11-24 11:27:18 +0200

    refactor(navbar): order-option component

--- src/app/view/pages/webstore/components/order-options/order-option/order-option.component.ts
+++ src/app/view/pages/webstore/components/order-options/order-option/order-option.component.ts
@@ -1,43 +1,29 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input, Signal, OnInit } from '@angular/core';
+import { Component, input, EventEmitter, Output, signal } from '@angular/core';
 import { PickupComponent } from '../pickup/pickup.component';
-import { DeliveryComponent } from "../delivery/delivery.component";
-import { DriveComponent } from "../drive/drive.component";
-import { PhonePopupComponent } from "../phone-popup/phone-popup.component";
-import { CodeVerificationComponent } from "../code-verification/code-verification.component";
+import { DeliveryComponent } from '../delivery/delivery.component';
+import { DriveComponent } from '../drive/drive.component';
+import { PhonePopupComponent } from '../phone-popup/phone-popup.component';
+
+type OrderOptionType = 'delivery' | 'pickup' | 'drive';
 
 @Component({
-  selector: 'app-order-option',
-  standalone: true,
-  imports: [CommonModule, PickupComponent, DeliveryComponent, DriveComponent, PhonePopupComponent, CodeVerificationComponent],
-  templateUrl: './order-option.component.html',
-  styleUrls: ['./order-option.component.scss']
+	selector: 'app-order-option',
+	standalone: true,
+	imports: [
+		CommonModule,
+		PickupComponent,
+		DeliveryComponent,
+		DriveComponent,
+		PhonePopupComponent,
+	],
+	templateUrl: './order-option.component.html',
+	styleUrls: ['./order-option.component.scss'],
 })
-export class OrderOptionComponent implements OnInit {
-	activeTab: string = 'delivery'; // Set initial active tab
-	hasRenderedOnce = false; // Flag to control initial animation
+export class OrderOptionComponent {
+	isVisible = input.required<boolean>();
 
-	@Input() isOrderOptionsVisible!: Signal<boolean>;
+	@Output() toggleVisibility = new EventEmitter<void>();
 
-	get isVisibleState(): boolean {
-	  return this.isOrderOptionsVisible();
-	}
-
-	@Input() toggleOrderOptionsVisibility!: () => void; // Function to toggle visibility
-
-	setActiveTab(tab: string) {
-	  this.activeTab = tab;
-	}
-
-	onButtonClick() {
-	  this.toggleOrderOptionsVisibility();
-	  console.log("details");
-	}
-
-	ngOnInit() {
-	  // Delay setting `hasRenderedOnce` to true to prevent the hide animation on initial load
-	  setTimeout(() => {
-		this.hasRenderedOnce = true;
-	  }, 0); // Sets the flag after the initial render cycle
-	}
-  }
+	activeTab = signal<OrderOptionType>('delivery'); // Set initial active tab
+}
