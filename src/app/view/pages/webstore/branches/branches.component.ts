e004f3a9cd9db332d18762f10a268aa6dca0a024
mrt <iquixe@gmail.com>
2024-11-25 02:56:38 +0200

    feat(branch): add branches page

---
+++ src/app/view/pages/webstore/branches/branches.component.ts
@@ -0,0 +1,13 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'branches',
+  standalone: true,
+  imports: [],
+  templateUrl: './branches.component.html',
+  styleUrl: './branches.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class BranchesComponent {
+
+}
