949da50f0c87fcebfc22200c59621333a4927a39
joesef1 <yousefel560@gmail.com>
2024-12-03 14:44:41 +0200

    feat(Unread-Notifications):Unread-Notifications-popup basic setup

--- src/app/view/pages/chat/components/nav-bar/nav-bar.component.ts
+++ src/app/view/pages/chat/components/nav-bar/nav-bar.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component,signal } from '@angular/core';
 import { CampaignIconComponent } from '@src/app/view/pages/chat/components/nav-bar/icons/campaign-icon.component';
 import { CaptainIconComponent } from '@src/app/view/pages/chat/components/nav-bar/icons/captain-icon.component';
 import { ContactsIconComponent } from '@src/app/view/pages/chat/components/nav-bar/icons/contacts-icon.component';
@@ -9,24 +9,41 @@ import { NotificationIconComponent } from '@src/app/view/pages/chat/components/n
 import { PortalsIconComponent } from '@src/app/view/pages/chat/components/nav-bar/icons/portals-icon.component';
 import { ReportsIconComponent } from '@src/app/view/pages/chat/components/nav-bar/icons/reports-icon.component';
 import { SettingsIconComponent } from '@src/app/view/pages/chat/components/nav-bar/icons/settings-icon.component';
+import { UnreadNotificationsComponent } from "./unread-notifications/unread-notifications.component";
 
 @Component({
 	selector: 'nav-bar',
 	standalone: true,
 	imports: [
-		InboxIconComponent,
-		ConversationIconComponent,
-		CampaignIconComponent,
-		CaptainIconComponent,
-		ContactsIconComponent,
-		NotificationIconComponent,
-		PortalsIconComponent,
-		ReportsIconComponent,
-		SettingsIconComponent,
-		MessageIconComponent,
-	],
+    InboxIconComponent,
+    ConversationIconComponent,
+    CampaignIconComponent,
+    CaptainIconComponent,
+    ContactsIconComponent,
+    NotificationIconComponent,
+    PortalsIconComponent,
+    ReportsIconComponent,
+    SettingsIconComponent,
+    MessageIconComponent,
+    UnreadNotificationsComponent
+],
 	templateUrl: './nav-bar.component.html',
 	styleUrl: './nav-bar.component.scss',
 	changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class NavBarComponent {}
+export class NavBarComponent {
+
+
+	// UnreadNotifications
+	showUnreadNotifications = signal(false); // Signal to control popup visibility
+
+  toggleUnreadNotifications() {
+    this.showUnreadNotifications.set(!this.showUnreadNotifications()); // Toggle popup visibility
+  }
+
+  handleUnreadNotificationsClose() {
+    this.showUnreadNotifications.set(false); // Close the popup
+  }
+	// ./UnreadNotifications
+
+}
---
+++ src/app/view/pages/chat/components/nav-bar/unread-notifications/unread-notifications.component.ts
@@ -0,0 +1,28 @@
+import {
+	ChangeDetectionStrategy,
+	Component,
+	signal,
+	Input,
+	Output,
+	EventEmitter,
+} from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { GearIconComponent } from '../icons/gear-icon.component';
+import { DismissIconComponent } from '../icons/dismiss-icon.component';
+
+@Component({
+	selector: 'unread-notifications',
+	standalone: true,
+	imports: [CommonModule, GearIconComponent, DismissIconComponent],
+	templateUrl: './unread-notifications.component.html',
+	styleUrl: './unread-notifications.component.scss',
+	changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class UnreadNotificationsComponent {
+	@Input() isVisible: boolean = false;
+	@Output() close = new EventEmitter<void>();
+
+	closePopup() {
+		this.close.emit();
+	}
+}
