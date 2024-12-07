904f046310b2fe2e0f6a6c11eba4bc0c1c06edae
joesef1 <yousefel560@gmail.com>
2024-12-07 18:50:16 +0200

    feat(chat/dashboard): manage the state of expanding and collapsing the conversation overview section

--- src/app/view/pages/chat/pages/dashboard/dashboard.component.ts
+++ src/app/view/pages/chat/pages/dashboard/dashboard.component.ts
@@ -1,13 +1,31 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { ConversationsOverviewComponent } from '../../components/dashboard/conversations-overview/conversations-overview/conversations-overview.component';
 import { ConversationDetailsComponent } from '../../components/dashboard/conversations-overview/conversation-details/conversation-details.component';
+import { animate, style, transition, trigger } from '@angular/animations';
+import { ConversationExpandCollapsService } from '../../services/dashboard/conversation-expand-collaps.service';
 
 @Component({
 	selector: 'dashboard',
 	standalone: true,
-	imports: [ConversationsOverviewComponent, ConversationDetailsComponent],
+	imports: [CommonModule ,ConversationsOverviewComponent, ConversationDetailsComponent],
 	templateUrl: './dashboard.component.html',
-	styleUrl: './dashboard.component.scss',
+	styleUrls: ['./dashboard.component.scss'],
 	changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class DashboardComponent {}
+export class DashboardComponent {
+	// Inject the scoped service
+	private ConversationExpandCollapsService = inject(
+		ConversationExpandCollapsService,
+	);
+
+	// Getter for expanded state
+	get isExpanded(): boolean {
+		return this.ConversationExpandCollapsService.isConversationsExpanded();
+	}
+
+	// Toggle the expanded state of conversations
+	toggleExpand(): void {
+		this.ConversationExpandCollapsService.toggleExpandConversations();
+	}
+}
