91bd7eaa6d92eaf31cdc8200196be82ab057045f
Yousef Elkholy <yousefel560@gmail.com>
2024-12-14 18:15:27 +0200

    feat(chat/dashboard/filter-popup): create the functionality of toggling the filter popup viability

--- src/app/view/pages/chat/components/dashboard/conversations-overview/filter-popup/filter-popup.component.ts
+++ src/app/view/pages/chat/components/dashboard/conversations-overview/filter-popup/filter-popup.component.ts
@@ -1,14 +1,22 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { DismissIconComponent } from "../../../nav-bar/icons/dismiss-icon.component";
-
+import { DismissIconComponent } from '../../../nav-bar/icons/dismiss-icon.component';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { FilterPopupToggleViabilityService } from './../../../../services/dashboard/filter-popup-toggle-viability.service';
 @Component({
-  selector: 'filter-popup',
-  standalone: true,
-  imports: [DismissIconComponent],
-  templateUrl: './filter-popup.component.html',
-  styleUrl: './filter-popup.component.scss',
-  changeDetection: ChangeDetectionStrategy.OnPush
+	selector: 'filter-popup',
+	standalone: true,
+	imports: [DismissIconComponent],
+	templateUrl: './filter-popup.component.html',
+	styleUrl: './filter-popup.component.scss',
+	changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class FilterPopupComponent {
+	// Inject the scoped service for filter popup
+	private filterPopupToggleViabilityService = inject(
+		FilterPopupToggleViabilityService,
+	);
 
+	// method for toggle the state for filter popup
+	toggleFilterPopup() {
+		this.filterPopupToggleViabilityService.toggleFilterPopup();
+	}
 }
--- src/app/view/pages/chat/components/dashboard/conversations-overview/sorting-filtering-section/sorting-filtering-section.component.ts
+++ src/app/view/pages/chat/components/dashboard/conversations-overview/sorting-filtering-section/sorting-filtering-section.component.ts
@@ -1,8 +1,9 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 
 import { SortIconComponent } from '../../icons/sort-icon.component';
 import { FilterIconComponent } from '../../icons/filter-icon.component';
-import { FilterPopupComponent } from "../filter-popup/filter-popup.component";
+import { FilterPopupComponent } from '../filter-popup/filter-popup.component';
+import { FilterPopupToggleViabilityService } from '../../../../services/dashboard/filter-popup-toggle-viability.service';
 
 @Component({
 	selector: 'sorting-filtering-section',
@@ -12,4 +13,17 @@ import { FilterPopupComponent } from "../filter-popup/filter-popup.component";
 	styleUrl: './sorting-filtering-section.component.scss',
 	changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class SortingFilteringSectionComponent {}
+export class SortingFilteringSectionComponent {
+	// Inject the scoped service for filter popup
+	FilterPopupToggleViabilityService = inject(
+		FilterPopupToggleViabilityService,
+	);
+	// getting the state for filter popup
+	get isFilterPopupVisible(): boolean {
+		return this.FilterPopupToggleViabilityService.isFilterPopupVisible();
+	}
+	// method for toggle the state for filter popup
+	toggleFilterPopup() {
+		this.FilterPopupToggleViabilityService.toggleFilterPopup();
+	}
+}
