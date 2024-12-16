9bf398556ecb0be4c89252583aadda15920be23c
mrt <iquixe@gmail.com>
2024-12-17 01:09:45 +0200

    chore(webstore/build): add post-build scripts

---
+++ scripts/post-local-webstore.cmd
@@ -0,0 +1,12 @@
+set "local_zip=C:\Users\m.rafat\source\vroot\dist\webstore\webstore.zip"
+set "local_dir=C:\Users\m.rafat\source\vroot\dist\webstore\browser"
+set "sz="C:\Program Files\7-Zip\7z.exe""
+set "ssh=C:\Windows\System32\OpenSSH\ssh.exe"
+set "scp=C:\Windows\System32\OpenSSH\scp"
+set "remote=root@10.99.77.244"
+
+del /f %local_zip%
+%sz% a %local_zip% %local_dir%\*
+
+%scp% %local_zip% %remote%:/var/www/
+%ssh% %remote% "rm -r /var/www/html/webstore; unzip /var/www/webstore.zip -d /var/www/html/webstore; rm -f /var/www/webstore.zip; reboot"
