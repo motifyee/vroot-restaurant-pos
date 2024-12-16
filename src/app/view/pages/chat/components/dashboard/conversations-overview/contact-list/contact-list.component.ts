04ae1be01fef07b2c72bcd6ff2f64c484ba370f5
Yousef Elkholy <yousefel560@gmail.com>
2024-12-16 21:33:06 +0200

    feat(chat/dashboard/conversation-overview):create basic functionality for toggling between contact types

--- src/app/view/pages/chat/components/dashboard/conversations-overview/contact-list/contact-list.component.ts
+++ src/app/view/pages/chat/components/dashboard/conversations-overview/contact-list/contact-list.component.ts
@@ -1,14 +1,20 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 import { ArrowLeftIconComponent } from '../../icons/arrow-left-icon.component';
 import { ExclamationIconComponent } from '../../icons/exclamation-icon.component';
 @Component({
-  selector: 'contact-list',
-  standalone: true,
-  imports: [ArrowLeftIconComponent,ExclamationIconComponent],
-  templateUrl: './contact-list.component.html',
-  styleUrl: './contact-list.component.scss',
-  changeDetection: ChangeDetectionStrategy.OnPush
+	selector: 'contact-list',
+	standalone: true,
+	imports: [ArrowLeftIconComponent, ExclamationIconComponent],
+	templateUrl: './contact-list.component.html',
+	styleUrl: './contact-list.component.scss',
+	changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ContactListComponent {
+	// Signal for active tab
+	activeTab = signal('mine'); // Default is 'mine'
 
+	// Method to change the active tab
+	setActiveTab(tab: string) {
+		this.activeTab.set(tab);
+	}
 }
