f9d95a73809c3d101da02849df9515e4e7ff68e4
Yousef Elkholy <yousefel560@gmail.com>
2024-12-17 19:07:52 +0200

    refactor(chat/dashboard/conversation-overview/filter-popup): replace static HTML with dynamic rendering using Angular control flow

--- src/app/view/pages/chat/components/dashboard/conversations-overview/filter-popup/filter-popup.component.ts
+++ src/app/view/pages/chat/components/dashboard/conversations-overview/filter-popup/filter-popup.component.ts
@@ -19,4 +19,34 @@ export class FilterPopupComponent {
 	toggleFilterPopup() {
 		this.filterPopupToggleViabilityService.toggleFilterPopup();
 	}
+
+	// dropdown options variable
+	dropdownOptions = [
+		{
+			label: 'Standard filters',
+			options: [
+				{ value: 'status', text: 'Status' },
+				{ value: 'assignee_id', text: 'Assignee name' },
+				{ value: 'inbox_id', text: 'Inbox name' },
+				{ value: 'team_id', text: 'Team name' },
+				{ value: 'display_id', text: 'Conversation identifier' },
+				{ value: 'campaign_id', text: 'Campaign name' },
+				{ value: 'labels', text: 'Labels' },
+				{ value: 'created_at', text: 'Created at' },
+				{ value: 'last_activity_at', text: 'Last activity' },
+			],
+		},
+		{
+			label: 'Additional filters',
+			options: [
+				{ value: 'browser_language', text: 'Browser language' },
+				{ value: 'country_code', text: 'Country name' },
+				{ value: 'referer', text: 'Referer link' },
+			],
+		},
+		{
+			label: 'Custom attributes',
+			options: [],
+		},
+	];
 }
