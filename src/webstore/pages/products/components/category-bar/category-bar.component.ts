104a81d9295f266cd31ac4fbe21a83baa10de619
mrt <iquixe@gmail.com>
2024-12-18 14:37:31 +0200

    fix(webstore/category-bar): highlight active category

--- src/webstore/pages/products/components/category-bar/category-bar.component.ts
+++ src/webstore/pages/products/components/category-bar/category-bar.component.ts
@@ -1,7 +1,15 @@
-import { Component, inject } from '@angular/core';
+import {
+	ChangeDetectionStrategy,
+	Component,
+	effect,
+	inject,
+	signal,
+} from '@angular/core';
 import { ScrollService } from '../../../../services/scroll.service';
 import { menuStore } from '@webstore/state';
 import { SkeletonModule } from 'primeng/skeleton';
+import { CategoryIntersectionObserver } from '../mobile-category-bar/category-intersection-observer';
+import { productsPageStore } from '../../products-page.store';
 
 @Component({
 	selector: 'category-bar',
@@ -9,17 +17,60 @@ import { SkeletonModule } from 'primeng/skeleton';
 	imports: [SkeletonModule],
 	templateUrl: './category-bar.component.html',
 	styleUrls: ['./category-bar.component.scss'],
+	changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CategoryBarComponent {
 	private menuStore = inject(menuStore);
 	private scrollService = inject(ScrollService);
+	productsPageStore = inject(productsPageStore);
 
-	activeCategoryId: string | null = null;
+	observer = new CategoryIntersectionObserver();
+
+	activeCategoryId = signal<number>(-1);
+
+	private readonly OBSERVER_REENABLE_DELAY = 800; // Delay before re-enabling observer
 
 	menu = this.menuStore.categories;
 
-	scrollToCategory(categoryIdx: string) {
-		this.activeCategoryId = categoryIdx; // Set active category
+	scrollToCategory(categoryIdx: number) {
+		this.activeCategoryId.set(categoryIdx); // Set active category
 		this.scrollService.tappedCategoryId.set(categoryIdx);
+
+		this.observer.skipIntersections = true;
+		setTimeout(() => {
+			this.observer.skipIntersections = false;
+		}, this.OBSERVER_REENABLE_DELAY);
 	}
+
+	// delay observer initialization until `products-page.component` ensures the categories has been initialized to view
+	_ = effect(() => {
+		if (!this.productsPageStore.categoriesViewHasInit()) return;
+
+		this.observer.initializeObserver('.category-section[category-id]');
+	});
+
+	ngOnDestroy(): void {
+		this.observer.disconnectObserver();
+	}
+
+	// Scrolls active category-tab into view
+	// on category-section intersection
+	__ = effect(
+		() => {
+			const categoryId = this.observer.intersectingCategoryId(),
+				tabId = `category-tab-${categoryId}`,
+				activeTab = document.getElementById(tabId);
+
+			if (!activeTab) return;
+
+			this.activeCategoryId.set(categoryId);
+
+			// activeTab.scrollIntoView({
+			// 	behavior: 'smooth',
+			// 	block: 'nearest',
+			// 	inline: 'center',
+			// });
+		},
+		{ allowSignalWrites: true },
+	);
 }
