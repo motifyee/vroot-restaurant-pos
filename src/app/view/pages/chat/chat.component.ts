69523ce45c9f3e10e0cf153b4a73cbf5824bfaa2
Yousef Elkholy <yousefel560@gmail.com>
2024-12-21 17:17:17 +0200

    refactor(chat):remove repeated button related classes and remap main css classes

--- src/app/view/pages/chat/chat.component.ts
+++ src/app/view/pages/chat/chat.component.ts
@@ -1,4 +1,8 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+	ChangeDetectionStrategy,
+	Component,
+	ViewEncapsulation,
+} from '@angular/core';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { RouterOutlet } from '@angular/router';
 
@@ -7,7 +11,12 @@ import { RouterOutlet } from '@angular/router';
 	standalone: true,
 	imports: [NavBarComponent, RouterOutlet],
 	templateUrl: './chat.component.html',
-	styleUrl: './chat.component.scss',
+	styleUrls: [
+		'./chat.component.scss',
+		'./style/styles.scss',
+		'./style/buttons.scss',
+	],
 	changeDetection: ChangeDetectionStrategy.OnPush,
+	encapsulation: ViewEncapsulation.None,
 })
 export class ChatComponent {}
--- tailwind.config.js
+++ tailwind.config.js
@@ -15,7 +15,38 @@ module.exports = {
 				'products-sm': 'repeat(auto-fit, minmax(8rem, 1fr))',
 				'products-lg': 'repeat(auto-fit, minmax(10rem, 1fr))',
 			},
+			backgroundColor: {
+				'white': '#ffffff', 
+				'transparent': 'transparent', 
+				'green-500': '#22C55E', 
+				'slate-700': '#5a6169', 
+				'slate-800': '#292b2e', 
+				'slate-900': '#101012', 
+			  },
+			  textColor: {
+				'slate-800': '#1E293B',
+				'slate-400': '#94A3B8',
+				'slate-100': '#F1F5F9',
+				'slate-200': '#E2E8F0',
+				'slate-300': '#CBD5E1',
+				'slate-500': '#64748B',
+				'black-900': '#000000',
+				
+
+			  },
+			  borderColor: {
+				'slate-50': '#F8FAFC',
+				'gray-300': '#D1D5DB',
+				'transparent': 'transparent',
+			  },
+			  hover: {
+				'bg-gray-300': '#D1D5DB',
+			  },
+			  active: {
+				'bg-stone-200': '#E7E5E4',
+			  },
 		},
+		
 	},
 	plugins: [],
 	safelist: ['border-2'],
