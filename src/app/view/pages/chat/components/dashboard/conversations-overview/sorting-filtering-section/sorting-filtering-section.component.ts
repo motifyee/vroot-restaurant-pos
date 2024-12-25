bd16e5b9a79ed25a74ac2f0986ac7e77ed6a8fc6
Yousef Elkholy <yousefel560@gmail.com>
2024-12-25 21:22:26 +0200

    feat(chat/dashboard/sort-popover):basic setup for sort-popover

--- src/app/view/pages/chat/components/dashboard/conversations-overview/sorting-filtering-section/sorting-filtering-section.component.ts
+++ src/app/view/pages/chat/components/dashboard/conversations-overview/sorting-filtering-section/sorting-filtering-section.component.ts
@@ -1,18 +1,47 @@
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import {
+	ChangeDetectionStrategy,
+	Component,
+	inject,
+	OnInit,
+} from '@angular/core';
 
 import { SortIconComponent } from '../../icons/sort-icon.component';
 import { FilterIconComponent } from '../../icons/filter-icon.component';
 import { FilterPopupComponent } from '../filter-popup/filter-popup.component';
 import { FilterPopupToggleViabilityService } from '../../../../services/dashboard/filter-popup-toggle-viability.service';
+import { Popover } from 'primeng/popover';
+import { InputGroup } from 'primeng/inputgroup';
+import { InputGroupAddonModule } from 'primeng/inputgroupaddon';
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+import { CommonModule } from '@angular/common';
+import { Select } from 'primeng/select';
+import { FormsModule } from '@angular/forms'; // Import FormsModule
 
+interface City {
+	name: string;
+	code: string;
+}
 @Component({
-    selector: 'sorting-filtering-section',
-    imports: [SortIconComponent, FilterIconComponent, FilterPopupComponent],
-    templateUrl: './sorting-filtering-section.component.html',
-    styleUrl: './sorting-filtering-section.component.scss',
-    changeDetection: ChangeDetectionStrategy.OnPush
+	selector: 'sorting-filtering-section',
+	imports: [
+		SortIconComponent,
+		FilterIconComponent,
+		FilterPopupComponent,
+		Popover,
+		//InputGroup,
+		InputGroupAddonModule,
+		ButtonModule,
+		InputTextModule,
+		CommonModule,
+		Select,
+		FormsModule,
+	],
+	templateUrl: './sorting-filtering-section.component.html',
+	styleUrl: './sorting-filtering-section.component.scss',
+	changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class SortingFilteringSectionComponent {
+export class SortingFilteringSectionComponent implements OnInit {
 	// Inject the scoped service for filter popup
 	FilterPopupToggleViabilityService = inject(
 		FilterPopupToggleViabilityService,
@@ -24,5 +53,18 @@ export class SortingFilteringSectionComponent {
 	// method for toggle the state for filter popup
 	toggleFilterPopup() {
 		this.FilterPopupToggleViabilityService.toggleFilterPopup();
+	}
+
+	cities: City[] | undefined;
+	selectedCity: City | undefined;
+
+	ngOnInit() {
+		this.cities = [
+			{ name: 'Open ', code: '1' },
+			{ name: 'Resolved', code: '2' },
+			{ name: 'Pending', code: '3' },
+			{ name: 'Resolved', code: '4' },
+			{ name: 'All', code: '5' },
+		];
 	}
 }
