6da5a2bb2a85502360f9c8604954fd63e6340118
Yousef Elkholy <yousefel560@gmail.com>
2024-12-28 08:51:33 +0200

    feat: create basic structure for team popover

--- src/app/view/pages/chat/components/dashboard/conversations-overview/conversation-bulk-action/components/team-popover/team-popover.component.ts
+++ src/app/view/pages/chat/components/dashboard/conversations-overview/conversation-bulk-action/components/team-popover/team-popover.component.ts
@@ -1,12 +1,24 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { SelectModule } from 'primeng/select';
 @Component({
-  selector: 'team-popover',
-  imports: [],
-  templateUrl: './team-popover.component.html',
-  styleUrl: './team-popover.component.scss',
-  changeDetection: ChangeDetectionStrategy.OnPush
+	selector: 'team-popover',
+	imports: [FormsModule, SelectModule],
+	templateUrl: './team-popover.component.html',
+	styleUrl: './team-popover.component.scss',
+	changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class TeamPopoverComponent {
+export class TeamPopoverComponent implements OnInit {
+	teams: any[] | undefined;
+	selectedTeam: string | undefined;
 
+	ngOnInit() {
+		this.teams = [
+			{ name: 'John Doe', id: 'A1' },
+			{ name: 'Jane Smith', id: 'A2' },
+			{ name: 'Mike Brown', id: 'A3' },
+			{ name: 'Emily Davis', id: 'A4' },
+			{ name: 'Chris Wilson', id: 'A5' },
+		];
+	}
 }
