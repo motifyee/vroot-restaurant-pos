6966325c9b82a8f14ca2464e68a3bf6567afd8dd
Yousef Elkholy <yousefel560@gmail.com>
2024-12-28 08:45:10 +0200

    feat: create the basic structure for the agent popover

--- src/app/view/pages/chat/components/dashboard/conversations-overview/conversation-bulk-action/components/agent-popover/agent-popover.component.ts
+++ src/app/view/pages/chat/components/dashboard/conversations-overview/conversation-bulk-action/components/agent-popover/agent-popover.component.ts
@@ -1,12 +1,25 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { SelectModule } from 'primeng/select';
 
 @Component({
-  selector: 'agent-popover',
-  imports: [],
-  templateUrl: './agent-popover.component.html',
-  styleUrl: './agent-popover.component.scss',
-  changeDetection: ChangeDetectionStrategy.OnPush
+	selector: 'agent-popover',
+	imports: [FormsModule, SelectModule],
+	templateUrl: './agent-popover.component.html',
+	styleUrl: './agent-popover.component.scss',
+	changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AgentPopoverComponent {
+export class AgentPopoverComponent implements OnInit {
+	agents: any[] | undefined;
+	selectedAgent: string | undefined;
 
+	ngOnInit() {
+		this.agents = [
+			{ name: 'John Doe', id: 'A1' },
+			{ name: 'Jane Smith', id: 'A2' },
+			{ name: 'Mike Brown', id: 'A3' },
+			{ name: 'Emily Davis', id: 'A4' },
+			{ name: 'Chris Wilson', id: 'A5' },
+		];
+	}
 }
