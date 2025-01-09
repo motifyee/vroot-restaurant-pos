705336df227a97985864eab933667018fe76be54
mrt <iquixe@gmail.com>
2025-01-09 12:37:51 +0200

    feat(webstore/orders-page): use api

--- webstore/pages/orders/orders.component.ts
+++ webstore/pages/orders/orders.component.ts
@@ -1,10 +1,27 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+	ChangeDetectionStrategy,
+	Component,
+	inject,
+	OnInit,
+} from '@angular/core';
+import { invoiceStore } from '@webstore/state';
+import { ButtonModule } from 'primeng/button';
+import { SkeletonModule } from 'primeng/skeleton';
 
 @Component({
-    selector: 'my-orders',
-    imports: [],
-    templateUrl: './orders.component.html',
-    styleUrl: './orders.component.scss',
-    changeDetection: ChangeDetectionStrategy.OnPush
+	selector: 'my-orders',
+	imports: [SkeletonModule, ButtonModule],
+	templateUrl: './orders.component.html',
+	styleUrl: './orders.component.scss',
+	changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class OrdersComponent {}
+export class OrdersComponent implements OnInit {
+	invoices = inject(invoiceStore);
+
+	ngOnInit(): void {
+		this.invoices.getInvoices({
+			pageNumber: 1,
+			pageSize: 10,
+		});
+	}
+}
